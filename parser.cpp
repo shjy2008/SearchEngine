c03c49f5a50eb05b12b40ef3fb315e93947541b4
shjy2008 <shjy2011@163.com>
2025-03-18 16:39:20 +1300

    update parser

--- parser.cpp
+++ parser.cpp
@@ -5,41 +5,144 @@
 // Extract words from a text string
 std::vector<std::string> extractWords(const std::string& text) {
 	std::vector<std::string> words;
-	std::istringstream stream(text);
 
 	std::string word;
-	while (stream >> word) {
-		std::string newWord = "";
-		for (size_t i = 0; i < word.length(); ++i) {
-			if (std::isalpha(word[i]))
-				newWord += std::tolower(word[i]);
+	for (size_t i = 0; i < text.size(); ++i){
+		if (std::isalnum(text[i]) || text[i] == '-') // some words have '-', such as "well-being"
+			word += std::tolower(text[i]);
+		else {
+			if (word.length() > 0) {
+				words.push_back(word);
+			}
+			word = "";
 		}
-		if (newWord.length() > 0)
-			words.push_back(newWord);
 	}
+	if (word.length() > 0)
+		words.push_back(word);
 
 	return words;
 }
 
-std::string stripString(const std::string& text) {
-	int start = 0;
-	int end = text.length() - 1;
+class Parser {
 
-	while (start <= end && isspace(text[start])) {
-		++start;
+private:
+	std::string fileName;
+
+public:
+	Parser(std::string fileName) {
+		this->fileName = fileName;
 	}
 
-	while (start <= end && isspace(text[end])) {
-		--end;
+	void runParser() {
+		std::ifstream file(this->fileName);
+		std::string line = "";
+
+		bool readingTag = false;
+		bool readingContent = false;
+
+		size_t readStartIndex = 0;
+		std::string currentTagName = "";
+		std::string currentText = "";
+		std::string currentDocNo = "";
+
+		int documentIndex = 0; // ++ when encounter </DOC>
+		int lineIndex = 0;
+
+		while (getline(file, line)) {
+
+			readStartIndex = 0;
+
+			for (size_t i = 0; i < line.length(); ++i) {
+
+				// Start of a tag
+				if (line[i] == '<') {
+					if (readingContent) {
+						std::string content = line.substr(readStartIndex, i - readStartIndex);
+						currentText += content;
+
+						// Extract and print all the words
+						std::vector<std::string> words = extractWords(currentText);
+
+						if (currentTagName == "DOCNO") { // the '<' of </DOCNO>, the close tag of a document no.
+							currentDocNo = words[0];
+							// Save the currentDocNo
+						}
+
+						// Output the words
+						for (size_t wordIndex = 0; wordIndex < words.size(); ++wordIndex) {
+							std::string word = words[wordIndex];
+							std::cout << word << std::endl; // output each word as a line
+						}
+
+						readingContent = false;
+					}
+
+					// Start to read tag
+					readingTag = true;
+					readStartIndex = i + 1;
+
+					continue;
+				}
+
+				// End of a tag
+				if (line[i] == '>') {
+					if (readingTag) {
+						std::string tagName = line.substr(readStartIndex, i - readStartIndex);
+						if (tagName[0] != '/') { // It's an open tag. e.g. <DOC>
+							currentTagName = tagName;
+						}
+						else { // It's a close tag. e.g. </DOC>
+
+							// Reach the end of a document
+							if (tagName == "/DOC") { 
+								currentDocNo = "";
+
+								// Output an blank line between documents
+								std::cout << std::endl; 
+
+								// if (documentIndex % 100 == 0) 
+								// {
+								// 	std::cout << documentIndex << " documents processed." << std::endl;
+								// }
+								++documentIndex;
+							}
+
+							currentTagName = "";
+							currentText = "";
+						}
+						
+						readingTag = false;
+					}
+
+					// Start to read content
+					readingContent = true;
+					readStartIndex = i + 1;
+
+					continue;
+				}
+			}
+
+			// Add the rest of the line to currentText
+			if (readingContent) {
+				std::string content = line.substr(readStartIndex, line.length() - readStartIndex);
+				currentText += content + "\n";
+			}
+
+			++lineIndex;
+
+			// TODO: for test
+			// if (lineIndex >= 100)
+			// 	break;
+		}
+
+		// std::cout << "All " << documentIndex << " documents processed." << std::endl;
+
 	}
-	return text.substr(start, end - start + 1);
-}
+};
 
 int main() {
-	std::ifstream file("wsj.xml");
 
-	std::string line = "";
-
+	// TODO: for test
 	// std::string testStr = "abcde abckk";
 	// std::vector<std::string> words = extractWords(testStr);
 	// std::cout << words[0] << words[1] << std::endl;
@@ -47,114 +150,8 @@ int main() {
 	// std::cout << "<" << stripString(s) << ">" << std::endl;
 	// return 0;
 
-	bool readingTag = false;
-	bool readingContent = false;
-
-	size_t readStartIndex = 0;
-	std::string currentTagName = "";
-
-	std::string currentText = "";
-
-	std::string currentDocNo = "";
-
-	int documentIndex = -1;
-
-
-	int lineIndex = 0;
-	while (getline(file, line)) {
-
-		readStartIndex = 0;
-
-		for (size_t i = 0; i < line.length(); ++i) {
-
-			// Start of a tag
-			if (line[i] == '<') {
-				if (readingContent) {
-					std::string content = line.substr(readStartIndex, i - readStartIndex);
-					currentText += content;
-
-					if (currentTagName != "DOCNO") // the '<' of </DOCNO>. (Don't need to extract texts of DOCNO)
-					{ 
-						// Extract and print all the words
-						std::vector<std::string> words = extractWords(currentText);
-						for (size_t wordIndex = 0; wordIndex < words.size(); ++wordIndex) {
-							std::string word = words[wordIndex];
-							std::cout << word << std::endl; // TODO: output each word as a line
-						}
-					}
-
-					// std::cout << content;
-					readingContent = false;
-				}
-
-				// Start to read tag
-				readingTag = true;
-				readStartIndex = i + 1;
-
-				continue;
-			}
-
-			// End of a tag
-			if (line[i] == '>') {
-				if (readingTag) {
-					std::string tagName = line.substr(readStartIndex, i - readStartIndex);
-					if (tagName[0] != '/') { // It's an open tag. e.g. <DOC>
-						currentTagName = tagName;
-						if (currentTagName == "DOC") { // The '>' of <DOC>, the beginning of a document
-							// if (documentIndex % 1000 == 0) 
-							// {
-							// 	std::cout << documentIndex << " documents processed." << std::endl;
-							// }
-							++documentIndex;
-						}
-					}
-					else { // It's a close tag. e.g. </DOC>
-
-						if (tagName == "/DOC") { // The '>' of </DOC>, the end of a document
-							currentDocNo = "";
-
-							std::cout << std::endl; // Output an blank line between documents
-						}
-						else if (currentTagName == "DOCNO") { // the '>' of </DOCNO>, the close tag of a document no.
-							currentDocNo = stripString(currentText);
-
-							// std::cout << currentDocNo << std::endl;
-						}
-
-						currentTagName = "";
-						currentText = "";
-					}
-					// std::cout << tagName;
-					readingTag = false;
-				}
-
-				// Start to read content
-				readingContent = true;
-				readStartIndex = i + 1;
-
-				continue;
-			}
-			// std::cout << line[lineIndex];
-		}
-
-		// Add the rest of the line to currentText
-		if (readingContent) {
-			std::string content = line.substr(readStartIndex, line.length() - readStartIndex);
-			// std::cout << content;
-			currentText += content + "\n";
-		}
-
-		// std::cout << std::endl;
-
-		// std::cout << line << std::endl;
-		++lineIndex;
-
-		// TODO
-		// if (lineIndex >= 50)
-		// 	break;
-	}
-
-	std::cout << "All " << documentIndex + 1 << " documents processed." << std::endl;
+	Parser parser("wsj.xml");
+	parser.runParser();
 
 	return 0;
 }
